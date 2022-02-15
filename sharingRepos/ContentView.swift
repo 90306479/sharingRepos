a344177f67b560b720d03d9f118b62a78ab68cc5
Haley Parker <buddza12@gmail.com>
2022-02-15 10:17:02 -0600

    Initial Commit

---
+++ sharingRepos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sharingRepos
+//
+//  Created by 90306479 on 2/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sharingRepos/sharingReposApp.swift
@@ -0,0 +1,17 @@
+//
+//  sharingReposApp.swift
+//  sharingRepos
+//
+//  Created by 90306479 on 2/15/22.
+//
+
+import SwiftUI
+
+@main
+struct sharingReposApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
